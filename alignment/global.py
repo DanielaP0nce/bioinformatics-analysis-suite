a40c648e8d37d0db7659256618b5b1306b7a94c3
DanielaP0nce <daniela_ponce_cartagena@brown.edu>
2025-03-29 02:47:07 -0400

    feat: starting to backtrack

--- alignment/global.py
+++ alignment/global.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 import numpy as np
+from alignment_constants import DIAGONAL, UP, LEFT
 
 
 def build_scoring_matrix(file):
@@ -20,9 +21,11 @@ def build_scoring_matrix(file):
 def initialize_alignment_table(file, gap_penalty):
     with open(file, "r") as text:
         text = text.readlines()
-        first_line = list(text[0].strip())
+        first_sequence = text[0].strip()
+        second_sequence = text[1].strip()
+        first_line = list(first_sequence)
         first_line.insert(0, "-")
-        second_line = list(text[1].strip())
+        second_line = list(second_sequence)
         second_line.insert(0, "-")
     alignment_table = pd.DataFrame(np.empty((len(second_line), len(first_line))))
     alignment_table.columns = first_line
@@ -36,12 +39,23 @@ def initialize_alignment_table(file, gap_penalty):
     alignment_table.iloc[0, 1:] = first_row_values
     alignment_table.iloc[1:, 0] = second_row_values
 
-    return alignment_table
+    pointer_table = pd.DataFrame(
+        0, index=alignment_table.index, columns=alignment_table.columns
+    )
+    pointer_table.iloc[0, 1:] = LEFT
+    pointer_table.iloc[1:, 0] = UP
+
+    return alignment_table, pointer_table, first_sequence, second_sequence
+
+
+# initialize_alignment_table("01.txt", -1)
 
 
 def needleman_wunsch(alignment_file, scoring_file, gap_penalty):
     scoring_table = build_scoring_matrix(scoring_file)
-    alignment_table = initialize_alignment_table(alignment_file, gap_penalty)
+    alignment_table, pointer_table, first_sequence, second_sequence = (
+        initialize_alignment_table(alignment_file, gap_penalty)
+    )
 
     for index in range(1, len(alignment_table.index)):
         for column in range(1, len(alignment_table.columns)):
@@ -53,11 +67,57 @@ def needleman_wunsch(alignment_file, scoring_file, gap_penalty):
             diagonal = int(alignment_table.iloc[index - 1, column - 1]) + match
             up = int(alignment_table.iloc[index - 1, column]) + gap_penalty
             left = int(alignment_table.iloc[index, column - 1]) + gap_penalty
-            alignment_table.iloc[index, column] = max(diagonal, up, left)
-    print(alignment_table)
+            max_score = max(diagonal, up, left)
+            alignment_table.iloc[index, column] = max_score
+
+            direction = 0
+            if diagonal == max_score:
+                direction |= DIAGONAL
+            if up == max_score:
+                direction |= UP
+            if left == max_score:
+                direction |= LEFT
+
+            pointer_table.iloc[index, column] = direction
+    aligned_seq1, aligned_seq2 = backtracking(
+        first_sequence, second_sequence, pointer_table
+    )
+    print("Alignment Score: ", alignment_table.iloc[-1, -1])
+    print("Aligned Sequences:")
+    print(aligned_seq1)
+    print(aligned_seq2)
+
+
+def backtracking(seq1, seq2, pointer_table):
+    i = len(pointer_table.index) - 1
+    j = len(pointer_table.columns) - 1
+
+    aligned_seq1 = ""
+    aligned_seq2 = ""
+
+    while i > 0 or j > 0:
+        current_pointer = pointer_table.iloc[i, j]
+        try:
+            if current_pointer & DIAGONAL and i > 0 and j > 0:
+                # Diagonal move (match/mismatch)
+                aligned_seq1 = seq1[i - 1] + aligned_seq1
+                aligned_seq2 = seq2[j - 1] + aligned_seq2
+                i -= 1
+                j -= 1
+            elif current_pointer & UP and i > 0:
+                # Up move (gap in seq2)
+                aligned_seq1 = seq1[i - 1] + aligned_seq1
+                aligned_seq2 = "-" + aligned_seq2
+                i -= 1
+            elif current_pointer & LEFT and j > 0:
+                # Left move (gap in seq1)
+                aligned_seq1 = "-" + aligned_seq1
+                aligned_seq2 = seq2[j - 1] + aligned_seq2
+                j -= 1
+
+        except:
+            raise ValueError("Invalid pointer value")
+    return aligned_seq1, aligned_seq2
 
 
 needleman_wunsch("01.txt", "standard.m", -1)
-
-
-
